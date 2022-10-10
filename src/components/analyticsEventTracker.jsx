cc2528d08e64b256d747d2b4ec8a62826ec66b2a
Ethan <62630748+ethanliu666@users.noreply.github.com>
2022-10-10 06:36:33 -0400

    new label name

--- src/components/analyticsEventTracker.jsx
+++ src/components/analyticsEventTracker.jsx
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga";
 
 function useAnalyticsEventTracker (category="Some category") {
-    const eventTracker = (action = "test action", label = "A label") => {
+    const eventTracker = (action = "test action", label = "a label") => {
       ReactGA.event({category, action, label});
     }
     return eventTracker;
