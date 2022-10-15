4167600dd9a61ec182e9029bbac2e3de9e2a9953
Ethan <62630748+ethanliu666@users.noreply.github.com>
2022-10-15 10:09:48 -0400

    added animations to contact sec

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,38 +4,51 @@ import email from "../assets/img/gmail.png";
 
 const Contact = () => {
   const gaEventTracker = useAnalyticsEventTracker("Contact");
-  const { ref, inView } = useInView({ triggerOnce: true });
+  const { ref: refOne, inView: oneInView } = useInView({ triggerOnce: true });
+  const { ref: refTwo, inView: twoInView } = useInView({ triggerOnce: true });
 
   return (
     <div
       name="contact"
       className="bg-storm flex flex-col items-center pb-20 px-5 md:px-24 lg:px-56 xl:px-80"
     >
-      <h2 className="text-cloud font-semibold uppercase text-center underline underline-offset-2 pb-3">
-        Contact
-      </h2>
-      <h1 className="font-bold text-center text-4xl text-slate-50 pb-5">
-        I may be an ISTJ, but...
-      </h1>
-      <p className="text-slate-50 text-lg text-center md:mx-20 xl:mx-72 pb-8">
-        That doesn't mean I don't like meeting new people or discussing new
-        ideas, so please, feel free to hit me up about any tech related projects
-        and/or ideas that you find are worth sharing. Hit me at ethanliu549@gmail.
-      </p>
-      <a
-        // style={{
-        //   backgroundImage: `url(${email})`,
-        //   backgroundPosition: "37% 50%",
-        //   backgroundRepeat: "no-repeat",
-        //   backgroundSize: "20px",
-        // }}
-        className="flex transition ease-in-out delay-150 hover:translate-x-[8px] w-max relative text-center text-slate-50 uppercase border-b-2 border-zinc-300 pb-1 untracking-wider"
-        href="mailto:ethanliu549@gmail.com"
-        target="_blank"
-      >
-        <img className="mr-3" src={email} alt="email icon" width="20px" height="20px"/>
-        Write me something
-      </a>
+      <div ref={refOne} className={oneInView ? "animatefu" : ""}>
+        <h2 className="text-cloud font-semibold uppercase text-center underline underline-offset-2 pb-3">
+          Contact
+        </h2>
+        <h1 className="font-bold text-center text-4xl text-slate-50 pb-5">
+          I may be an ISTJ, but...
+        </h1>
+        <p className="text-slate-50 text-lg text-center mx-8 md:mx-20 xl:mx-72 pb-8">
+          That doesn't mean I don't like meeting new people or discussing new
+          ideas, so please, feel free to hit me up about any tech related
+          projects and/or ideas that you find are worth sharing. Hit me at
+          ethanliu549@gmail.
+        </p>
+      </div>
+      <div ref={refTwo} className={twoInView? "animatefr": ""}>
+        <a
+          // style={{
+          //   backgroundImage: `url(${email})`,
+          //   backgroundPosition: "37% 50%",
+          //   backgroundRepeat: "no-repeat",
+          //   backgroundSize: "20px",
+          // }}
+          className="flex transition ease-in-out delay-150 hover:translate-x-[8px] w-max relative text-center text-slate-50 uppercase border-b-2 border-zinc-300 pb-1 untracking-wider"
+          href="mailto:ethanliu549@gmail.com"
+          target="_blank"
+          onClick={() => gaEventTracker("Email")}
+        >
+          <img
+            className="mr-3"
+            src={email}
+            alt="email icon"
+            width="20px"
+            height="20px"
+          />
+          Write me something
+        </a>
+      </div>
     </div>
   );
 };
