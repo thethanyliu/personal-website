0aacefd2fe7b1b2d43d1e2be34371831f55509d6
Ethan <62630748+ethanliu666@users.noreply.github.com>
2022-11-01 16:26:00 -0400

    updated animation css

--- src/components/Locations/Locations.jsx
+++ src/components/Locations/Locations.jsx
@@ -1,44 +1,40 @@
-import { useInView } from "react-intersection-observer";
 import { useState } from "react";
 import Map from "./Map";
 import Timeline from "./Timeline";
 
 function Locations() {
   const [toggle, setToggle] = useState(false);
-  const { ref, inView } = useInView({ triggerOnce: true });
 
   function handleToggle() {
     setToggle(!toggle);
   }
 
   return (
-    <div name="places" className="flex flex-col bg-storm py-8 px-5 md:px-24 lg:px-56 xl:px-80 location">
-      <div
-        ref={ref}
-        className={inView ? "animatefd" : ""}
-      >
-        <>
-          <h1 className="text-4xl font-semibold text-center text-slate-50 py-10">
-            Where I've Lived
-          </h1>
+    <div
+      name="places"
+      className="flex flex-col bg-storm py-8 px-5 md:px-24 lg:px-56 xl:px-80 location"
+    >
+      <>
+        <h1 className="text-4xl font-semibold text-center text-slate-50 py-10">
+          Where I've Lived
+        </h1>
 
-          <div
-            className={`toggle${toggle ? " second" : ""}`}
-            onClick={handleToggle}
-          >
-            <div className="notch"></div>
-          </div>
-          <div className="text-center text-slate-50 text-2xl pt-3">
-            {!toggle ? "See Timeline" : "See Map"}
-          </div>
-        </>
+        <div
+          className={`toggle${toggle ? " second" : ""}`}
+          onClick={handleToggle}
+        >
+          <div className="notch"></div>
+        </div>
+        <div className="text-center text-slate-50 text-2xl pt-3">
+          {!toggle ? "See Timeline" : "See Map"}
+        </div>
+      </>
 
-        <div className={!toggle ? "" : "hidden"}>
-          <Map />
-        </div>
-        <div className={toggle ? "" : "hidden"}>
-          <Timeline />
-        </div>
+      <div className={!toggle ? "" : "hidden"}>
+        <Map />
+      </div>
+      <div className={toggle ? "" : "hidden"}>
+        <Timeline />
       </div>
     </div>
   );
