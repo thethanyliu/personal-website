7b0e7eee55d20164657cf8458b8da2147876083a
Ethan <62630748+ethanliu666@users.noreply.github.com>
2022-11-10 22:37:35 -0500

    added typing text

--- src/components/IntroSection.jsx
+++ src/components/IntroSection.jsx
@@ -1,9 +1,10 @@
 import useAnalyticsEventTracker from "./analyticsEventTracker";
+import Typical from "react-typical";
 import BG from "../assets/img/BGC.png";
 import Bgif from "../assets/img/bgif.gif";
 
 function IntroSection() {
-  const gaEventTracker = useAnalyticsEventTracker("Rick Roll Counter")
+  const gaEventTracker = useAnalyticsEventTracker("Rick Roll Counter");
 
   return (
     <div
@@ -17,7 +18,7 @@ function IntroSection() {
     >
       <div className="grid md:grid-cols-2 justify-between w-full h-screen px-0 lg:px-24 xl:px-64">
         <div className="flex flex-col relative justify-center items-start pl-4 md:pl-0 m-auto">
-        <span className="z-10 text-md text-left font-bold text-slate-50 w-full">
+          <span className="z-10 text-md text-left font-bold text-slate-50 w-full">
             Hello there, I'm
           </span>
           <span className="text-5xl lg:text-6xl text-left font-bold text-slate-50 pb-3 w-full z-10">
@@ -27,14 +28,18 @@ function IntroSection() {
             For those of you on desktop, yes that is my bitmoji, so please add
             me on snap at ethanliu666.
           </span>
-          <span className="text-[4rem] lg:text-[6rem] absolute top-[-30px] lg:top-[-50px] z-2 font-bold text-[#113245] tracking-wide">
-            Developer
+          <span className="absolute top-[-30px] lg:top-[-50px] text-[4rem] lg:text-[6rem] font-bold z-2 text-[#113245] tracking-wide">
+            <Typical
+              steps={["Software", 1500, "Devploper", 2000]}
+              loop={Infinity}
+              wrapper="b"
+            />
           </span>
           <button className="transition ease-in-out delay-150 hover:translate-x-[8px] text-left text-slate-50 uppercase tracking-wider underline underline-offset-8 py-3">
             <a
               href="https://www.youtube.com/watch?v=O91DT1pR1ew"
               target="_blank"
-              onClick={()=>gaEventTracker("Find out more")}
+              onClick={() => gaEventTracker("Find out more")}
             >
               Find out more
             </a>
