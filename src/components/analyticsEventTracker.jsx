6d82203a1f0690f45bff9390c3a730c9064c294a
Ethan Liu <62630748+ethanliu666@users.noreply.github.com>
2023-06-22 15:44:48 -0600

    updated ga4

--- src/components/analyticsEventTracker.jsx
+++ src/components/analyticsEventTracker.jsx
@@ -1,6 +1,6 @@
 import ReactGA from "react-ga4";
 
-function useAnalyticsEventTracker(category="Some category", action) {
+function useAnalyticsEventTracker(category="Some category", action="Some action") {
   ReactGA.event({
     category: category,
     action: action
