228785c9f26d508ab8f8629c19fc8aeb58fbe357
Ethan Liu <ethanliu549@gmail.com>
2025-06-14 23:10:23 -0400

    remove content

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,13 +1,6 @@
-import { analyticsEventTracker } from "../utils/analyticsEventTracker";
 import { Link } from "react-scroll";
-import Instalight from "../assets/logos/gramm.png";
-import Instadark from "../assets/logos/insta-logo-dark.png";
 
 function AboutSection() {
-  const instaButtonClick = () => {
-    analyticsEventTracker("Instagram", "Clicked Insta");
-  };
-
   return (
     <div
       name="about"
@@ -25,29 +18,6 @@ function AboutSection() {
           <span className="dark:text-sand"> Software Engineer</span> & CS
           @University of Waterloo
         </span>
-        <a
-          className="flex transition ease-in-out delay-150 hover:translate-x-[8px] ml-2 pt-5 pb-1 text-left uppercase tracking-wider border-b-2 dark:border-white border-stone-800 cursor-pointer"
-          href="https://www.instagram.com/ethanliu8/"
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={() => instaButtonClick()}
-        >
-          <span className="text-md">Say hello</span>
-          <img
-            className="hidden dark:flex ml-3"
-            src={Instalight}
-            alt="Instagram"
-            width="20px"
-            height="20px"
-          />
-          <img
-            className="flex ml-3 dark:hidden"
-            src={Instadark}
-            alt="Instagram"
-            width="20px"
-            height="20px"
-          />
-        </a>
       </div>
       <div className="flex my-8 mx-5">
         <p className="text-lg">
