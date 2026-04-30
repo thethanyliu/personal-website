d9fb4c99f4d1e4c4c1c94b6fb0f46934c69a9a46
Ethan Liu <ethanliu549@gmail.com>
2026-04-29 20:39:17 -0600

    update about section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,12 +1,10 @@
-import { Link } from "react-scroll";
-
 function AboutSection() {
   return (
     <div
       name="about"
       className="dark:bg-teal bg-[#c0c2ce] grid md:grid-cols-2 justify-center items-center px-5 md:px-16 lg:px-52 xl:px-72"
     >
-      <div className="flex flex-col items-start mt-10 mx-5 md:my-[3.5rem]">
+      <div className="flex flex-col items-start mt-10 mx-8 md:my-[3.5rem]">
         <h3 className="text-left font-semibold uppercase text-md ml-2 pb-3 underline underline-offset-2 dark:text-cloud text-[#4c4c55]">
           About Me
         </h3>
@@ -21,15 +19,9 @@ function AboutSection() {
       </div>
       <div className="flex my-8 mx-5">
         <p className="text-lg">
-          Currently a third year CS student at the University of Waterloo.
-          That&rsquo;s it, that&rsquo;s really all I have to say, but do feel
-          free to hang around as long as you like and always do feel free to{" "}
-          <Link to={"contact"} smooth={true} duration={500}>
-            <span className="cursor-pointer font-semibold underline">
-              reach out
-            </span>
-          </Link>
-          .
+          Currently a fourth year CS and statistics student at the University of
+          Waterloo. That&rsquo;s it, that&rsquo;s really all I have to say, but
+          do feel free to hang around as long as you like.
         </p>
       </div>
     </div>
